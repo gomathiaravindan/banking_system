3b5346925e7b954be3b56aa05a9b5c589feb6f87
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-02-04 11:42:08 +0530

    Add files via upload

---
+++ php/connection.php
@@ -0,0 +1,11 @@
+<?php
+  
+		 $host = "localhost";
+		 $db ="id15453805_bank_db";
+		 $user="id15453805_localhost";
+		 $pwd="@/ErKNu(VbjbeI7Q";
+		 $conn =mysqli_connect($host,$user,$pwd,$db);
+		 if($conn){
+		 	echo "Connected Successfully!!";
+		 }
+		 ?>
